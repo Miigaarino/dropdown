62e7ba006b35ac9b6491d8e6a886c3b92b32b29e
Miigaarino <miner_miga@yahoo.com>
2021-06-15 14:46:26 +0800

    Shieet

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,20 @@ import styles from '../styles/Home.module.css'
 import info from '../data/sumDuureg'
 const db = require('../data/sumDuuregS.json')
 
-const [hotAimag, sethotAimag] = useState('')
-
 const data = (name) => {
   return db[name]
 }
 
 export default function Home() {
+  const [hotAimag, sethotAimag] = useState('')
+
   return (
     <div className='main'>
-      {console.log(db['Ulaanbaatar'].ner)}
-      <select name='' id=''></select>
+      <select>
+        {Object.keys(db).map((key) => (
+          <option>{key}</option>
+        ))}
+      </select>
     </div>
   )
 }

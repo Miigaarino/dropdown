65c96ba751c9e9fa05aa2cd1d0366806067cde03
Miigaarino <miner_miga@yahoo.com>
2021-06-17 14:53:17 +0800

    useReducer Done.

--- pages/index.js
+++ pages/index.js
@@ -4,54 +4,114 @@ import Select from 'react-select'
 import data from '../data/data'
 // const db = require('../data/sumDuuregS.json')
 
-const SET_SUM_DUUREG_LIST = 'setSumDuuregList'
-const SET_BAG_KHOROO_LIST = 'setBagKhorooList'
 const SET_AIMAG_KHOT = 'setAimagKhot'
 const SET_SUM_DUUREG = 'setSumDuureg'
 const SET_BAG_KHOROO = 'setBagKhoroo'
 
-const initialState = {
-  sumDuuregList: [],
-  bagKhorooList: [],
-  aimagKhot: '',
-  sumDuureg: '',
-  bagKhoroo: '',
-}
-
-function reducer(state, action) {
+const reducer = (state, action) => {
   switch (action.type) {
-    case SET_SUM_DUUREG_LIST:
-      return {}
-    case SET_BAG_KHOROO_LIST:
-      return {}
     case SET_AIMAG_KHOT:
-      return { aimagKhot: state.aimagKhot }
+      return {
+        // ...state,
+        aimagKhot: action.index,
+        sumDuureg: null,
+        bagKhoroo: null,
+      }
     case SET_SUM_DUUREG:
-      return { sumDuureg: state.sumDuureg }
+      return { ...state, sumDuureg: action.index, bagKhoroo: null }
     case SET_BAG_KHOROO:
-      return { bagKhoroo: state.bagKhoroo }
+      return { ...state, bagKhoroo: action.index }
     default:
-      return initialState
+      return state
   }
 }
 
 export default function Home() {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, {
+    aimagKhot: null,
+    sumDuureg: null,
+    bagKhoroo: null,
+  })
+
+  const handleAimagKhotChange = (index) => {
+    dispatch({ type: SET_AIMAG_KHOT, index })
+  }
+  const handleSumDuuregChange = (index) => {
+    dispatch({ type: SET_SUM_DUUREG, index })
+  }
+  const handleBagKhorooChange = (index) => {
+    dispatch({ type: SET_BAG_KHOROO, index })
+  }
 
   return (
     <div className={styles.container}>
       <main>
         <h1>Even better local version</h1>
 
+        <h1>Аймаг хот</h1>
         <Select
-          instanceId='1'
-          options={data}
-          value={state.aimagKhot}
+          instanceId='khotAimag'
+          options={data.map((i, index) => ({ ...i, index }))}
+          value={
+            state.aimagKhot != null
+              ? data.map((i, index) => ({ ...i, index }))[state.aimagKhot]
+              : []
+          }
           isSearchable
           placeholder='Аймаг/Хот сонгоно уу'
-          // onChange={(e) => {
-          //   dispatch({ type: SET_SUM_DUUREG_LIST })
-          // }}
+          onChange={(e) => handleAimagKhotChange(e.index)}
+          // getOptionValue={(i) => i.index}
+          // getOptionLabel={(i) => i.label}
+        />
+        <h1>Сум дүүрэг</h1>
+        <Select
+          instanceId='sum'
+          options={
+            state.aimagKhot != null
+              ? data[state.aimagKhot].sumDuurguud.map((i, index) => ({
+                  ...i,
+                  index,
+                }))
+              : []
+          }
+          value={
+            state.sumDuureg != null
+              ? data[state.aimagKhot].sumDuurguud.map((i, index) => ({
+                  ...i,
+                  index,
+                }))[state.sumDuureg]
+              : []
+          }
+          isSearchable
+          placeholder='Сум/Дүүрэг сонгоно уу'
+          onChange={(e) => handleSumDuuregChange(e.index)}
+          // getOptionValue={(i) => i.index}
+        />
+        <h1>Баг хороо</h1>
+        <Select
+          instanceId='3'
+          options={
+            state.sumDuureg != null
+              ? data[state.aimagKhot].sumDuurguud[
+                  state.sumDuureg
+                ].bagKhorood.map((i, index) => ({ ...i, index }))
+              : []
+          }
+          value={
+            state.bagKhoroo != null
+              ? data[state.aimagKhot].sumDuurguud[
+                  state.sumDuureg
+                ].bagKhorood.map((i, index) => ({ ...i, index }))[
+                  state.bagKhoroo
+                ]
+              : []
+          }
+          isSearchable
+          placeholder='Сум/Дүүрэг сонгоно уу'
+          onChange={(e) => {
+            handleBagKhorooChange(e.index)
+          }}
+          // getOptionValue={(i) => i.index}
         />
       </main>
     </div>
